db2ad6744610c835c1f2e0ac703dc8fa817110b9
ju0 <0802ryu@gmail.com>
2022-05-17 11:46:50 +0900

    dashBoardRestController

--- src/main/java/com/solarpred/solcaster/mapper/DashBoardMapper.java
+++ src/main/java/com/solarpred/solcaster/mapper/DashBoardMapper.java
@@ -1,11 +1,17 @@
 package com.solarpred.solcaster.mapper;
 
+import com.solarpred.solcaster.domain.DashBoard;
 import com.solarpred.solcaster.domain.Temp_weather;
+
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
 
 @MapperScan
 public interface DashBoardMapper {
 
     public Temp_weather getTemp_weather(String parsingTime);
+    
+    public List<DashBoard> DashBoardSelect();
 
 }
